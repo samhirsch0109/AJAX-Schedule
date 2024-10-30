c414b9a16935ec8d0902957f2f19bc2b94d9e15d
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-10-30 00:06:02 +0000

    kind of works

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const btn = $(scheduleDay)
+$(document).ready(function () {
+    const scheduleUrl = 'https://api.npoint.io/d62afb01423e308e9037';
 
-btn.on('click', function () {
-    $.ajax({
-        url: 'https://api.npoint.io/d62afb01423e308e9037',
-        method: 'GET',
-        success: function (data) {
-            const schedule = data.schedule
-            function getClassesForDay(day) {
-                return scheduleData.filter(item => item.days.includes(selectedDay));
-            }
+    $('#submitDay').on('click', function () {
+        const selectedDay = $('#dayInput').val().trim().toUpperCase();
+
+        if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
+            alert('You need to type a valid letter day!');
+            return;
+        } else {
+            $.ajax({
+                url: 'https://api.npoint.io/d62afb01423e308e9037',
+                method: 'GET',
+                success: function (data) {
+                    let schedule = data.schedule;
+                    let daySchedule = schedule.filter(classItem => classItem.days.includes(selectedDay));
+                    renderHTML(daySchedule);
+                },
+
+                error: function () {
+                    console.log('Connection error');
+                }
+            });
+        }
+    });
+
+    function renderHTML(data) {
+        $('#scheduleList').empty();
+        data.forEach(classItem => {
+
+            $('#scheduleList').append(`
+                <tr>
+                    <td>${classItem.period}</td>
+                    <td>${classItem.time}</td>
+                    <td>${classItem.class}</td>
+                    <td>${classItem.teacher}</td>
+                    <td>${classItem.room}</td>
+                </tr>
+            `);
         });
+    }
 });
-
-
-
-
