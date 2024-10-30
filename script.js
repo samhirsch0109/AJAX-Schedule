ee257278dbb1a78cd08fd6c133cc73695195f4f0
samhirsch0109 <sammyhirsch0109@gmail.com>
2024-10-30 10:42:34 -0400

    making progress

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
     const scheduleUrl = 'https://api.npoint.io/d62afb01423e308e9037';
 
+    const bellSchedule = {
+        1: { start: '8:24 AM', end: '9:31 AM' },
+        2: { start: '9:36 AM', end: '10:43 AM' },
+        3: { start: '10:48 AM', end: '11:55 AM' },
+        4: { start: '12:41 PM', end: '1:48 PM' },
+        5: { start: '1:53 PM', end: '3:00 PM' },
+    }
+
     $('#submitDay').on('click', function () {
         const selectedDay = $('#dayInput').val().trim().toUpperCase();
 
         if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
             alert('You need to type a valid letter day!');
             return;
+
         } else {
             $.ajax({
                 url: 'https://api.npoint.io/d62afb01423e308e9037',
@@ -27,11 +36,9 @@ $(document).ready(function () {
     function renderHTML(data) {
         $('#scheduleList').empty();
         data.forEach(classItem => {
-
             $('#scheduleList').append(`
                 <tr>
                     <td>${classItem.period}</td>
-                    <td>${classItem.time}</td>
                     <td>${classItem.class}</td>
                     <td>${classItem.teacher}</td>
                     <td>${classItem.room}</td>
